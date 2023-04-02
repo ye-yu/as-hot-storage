54223c4b7d6e93a308e1cf2c1d5e9755799494e1
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-04-02 20:59:09 +0800

    Set int storage as i32

--- assembly/storages.ts
+++ assembly/storages.ts
@@ -2,7 +2,7 @@ import { KeyInfo } from "./KeyInfo";
 import { TypedStoredValue } from "./StoredValue";
 
 export const stringStorage = new Map<string, TypedStoredValue<string>>();
-export const intStorage = new Map<string, TypedStoredValue<i64>>();
+export const intStorage = new Map<string, TypedStoredValue<i32>>();
 export const floatStorage = new Map<string, TypedStoredValue<f64>>();
 export const keyInfos = new Map<string, KeyInfo>();
 export const expiringKeyStorage = new Set<KeyInfo>();
